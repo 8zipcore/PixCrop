d5737cb6747d41b1bda40a7743cbd80fe4b94bf2
SEUNGA HONG <howtofindjayu@gmail.com>
2025-09-18 19:58:23 +0900

    feat: Package.swift 추가

---
+++ PixCrop/Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "PixCrop",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "PixCrop",
+            targets: ["PixCrop"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.0.0")
+    ],
+    targets: [
+        .target(
+            name: "PixCrop",
+            dependencies: [
+                "SnapKit"
+            ],
+            path: "Sources/PixCrop",
+            resources: [
+                .process("../../Assets")
+            ]
+        ),
+        .testTarget(
+            name: "PixCropTests",
+            dependencies: ["PixCrop"],
+            path: "Tests/PixCropTests"
+        )
+    ]
+)
